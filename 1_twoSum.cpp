2f786111622a4f6170b376ee76d76258695b3a41
JohnWZ <johnzhaiisgreat@gmail>
2020-05-06 23:20:23 +0930

    question1

---
+++ 1_twoSum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i=0; i<nums.size();i++)
+            for (int j = i+1; j<nums.size();j++)
+                if (nums[i]+nums[j]==target)
+                    return {i,j}; // return an array list directly
+        return {}; // return an empty array list if there is no answer
+    }
+};
+
+int main()
+{
+	Solution s = Solution();
+	return 0;
+}
---
+++ 1_two_sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for num1 in List:
+            for num2 in List:
+                if num1 + num2 == target and num1 != num2:
+                    return arr[num1, num2]
