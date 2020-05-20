a17eed8a75d1b8556c969e9c24a4a035d342e58d
JohnWZ <johnzhaiisgreat@gmail.com>
2020-05-20 20:33:53 +0930

    q46

--- 46_Permutations.py
+++ 46_Permutations.py
@@ -1,2 +1,38 @@
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute(self, nums):
+        if len(nums) <= 1:
+            return [nums]
+
+        answer = []
+        # enumerate() can return both the index and the value of the number
+        for i, num in enumerate(nums):
+            # n is a list which contains all the elements of nums except i
+            # 依次选择list中所有的元素作为开头，剩下的进入下一轮递归
+            n = nums[:i] + nums[i+1:]
+            # j will be a result of permute(n), get all results with for loop
+            for j in self.permute(n):
+                # append j to the list [num] which contains only one elment num
+                answer.append([num] + j)
+
+        return answer
+
+def main():
+    arr= []
+    l = int(input("Length of the array:"))
+    for i in range(l):
+        elem = int(input(f"arr[{i}]:"))
+        arr.append(elem)
+
+    s = Solution()
+    print(s.permute(arr))
+    while True:
+        arr.clear()
+        l = int(input("Length of the array:"))
+        for i in range(l):
+            elem = int(input(f"arr[{i}]:"))
+            arr.append(elem)
+        print(s.permute(arr))
+
+
+if __name__ == '__main__':
+    main()
